d2b19c09c55ad180cbe8bdb362d67efae9fffc2d
Cynthia Powell <aruchty@value.net>
2019-11-13 18:47:24 +0000

    adjust spec file

--- spec/binarytree_spec.rb
+++ spec/binarytree_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 RSpec.describe BinaryTree, type: :model do
   describe "binarytree sort should work" do
     it "should order the array" do
+      
       expected = [1, 4, 6, 7, 9, 10, 14]
       array = [7, 4, 9, 1, 6, 14, 10]
+
       mySort = BinaryTreeSort.new
       expect(mySort.sort(array)).to eq expected
     end
